97a6fa618d2d4e6dcd023b605d1ac5e18a6c6f80
KJ Price <kjprice12@gmail.com>
2024-07-23 08:42:09 -0500

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -15,21 +15,22 @@ import LeafletMapComponent from './components/LeafletMap';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import store from './store';
 import theme from './theme';
-import { setRoutes } from './actions/routes';
+import { setRoutes, setRoutesError } from './actions/routes';
 
 const queryClient = new QueryClient();
+
+const fetchRoutes = () => {
+	return fetch('/api/routes')
+		.then((res) => res.json())
+		.then((routes) => store.dispatch(setRoutes(routes)))
+		.catch((e) => store.dispatch(setRoutesError(routes)));
+};
 
 export default function App({ Component }) {
 	useEffect(() => {
 		// TODO: #5 - fix race conditions
 		// TODO: This should likely be set in thunk, saga, or use middleware api
-
-		fetch('/api/routes')
-			.then((res) => res.json())
-			.then((routes) => store.dispatch(setRoutes(routes)))
-			.catch((e) =>
-				store.dispatch({ type: 'SET_ROUTES', payload: routes })
-			);
+		fetchRoutes();
 	}, []);
 
 	const state = store.getState();
