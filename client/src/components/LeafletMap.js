0925549aeb421ece07511b29fdc601aadc5cfa46
KJ Price <kjprice12@gmail.com>
2024-07-23 09:18:12 -0500

    Step 1 - add color to lines

--- client/src/components/LeafletMap.js
+++ client/src/components/LeafletMap.js
@@ -1,8 +1,13 @@
+import { color } from '@chakra-ui/react';
 import React from 'react';
 import { MapContainer, Marker, Popup, TileLayer, GeoJSON } from 'react-leaflet';
 import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
 
 export default function LeafletMap({ position }) {
+	const { routeColorsById } = useSelector((state) => {
+		return state.routes;
+	});
 	const { data } = useQuery('shapes', () =>
 		fetch('/api/shapes').then((res) => res.json())
 	);
@@ -19,7 +24,15 @@ export default function LeafletMap({ position }) {
 			/>
 			{(data || []).map((route) => {
 				return route.shapes.map((shape, i) => {
-					return <GeoJSON key={`subway-lines_${i}`} data={shape} />;
+					return (
+						<GeoJSON
+							key={`subway-lines_${i}`}
+							data={shape}
+							style={{
+								color: routeColorsById[route.route_id] || 'red',
+							}}
+						/>
+					);
 				});
 			})}
 		</MapContainer>
--- client/src/reducers/routes.js
+++ client/src/reducers/routes.js
@@ -3,17 +3,29 @@ import { SET_ROUTES, SET_ROUTES_ERROR } from '../actions/routes';
 const defaultState = {
 	error: null,
 	routes: null,
+	routeColorsById: {},
+};
+
+const setRouteColorsById = (routes) => {
+	const routeColorsById = {};
+	routes.forEach(({ route_id, route_color }) => {
+		routeColorsById[route_id] = '#' + route_color;
+	});
+	return routeColorsById;
 };
 
 const RoutesReducer = (state = defaultState, action) => {
 	switch (action.type) {
 		case SET_ROUTES:
 			return {
+				...state,
 				error: null,
+				routeColorsById: setRouteColorsById(action.payload),
 				routes: action.payload,
 			};
 		case SET_ROUTES_ERROR:
 			return {
+				...state,
 				routes: null,
 				error: action.payload,
 			};
